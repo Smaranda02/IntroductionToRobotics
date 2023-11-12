451a45771afa3897794af03ad297f7f6584e3124
Smaranda02 <62556419+Smaranda02@users.noreply.github.com>
2023-11-12 18:41:10 +0200

    Update Homework1-RGB.ino

--- Homework1-RGB.ino
+++ Homework1-RGB.ino
@@ -4,23 +4,25 @@ const int blueLedPin = 11;
 const int redLedPin = 9;
 const int greenLedPin = 10;
 
-const int variableResistorPinForBlueLed = A0;
-const int variableResistorPinForRedLed = A2;
-const int variableResistorPinForGreenLed = A1;
+const int blueLedPotentiometerPin = A0;
+const int redLedPotentiometerPin = A2;
+const int greenLedPotentiometerPin = A1;
 
-int variableResistorValueForBlueLed = 0;
-int variableResistorValueForRedLed = 0;
-int variableResistorValueForGreenLed = 0;
+int blueLedPotentiometer = 0;
+int redLedPotentiometer = 0;
+int greenLedPotentiometer = 0;
 
 float voltageBlue = 0;
 float voltageRed = 0;
 float voltageGreen = 0;
 
-int maxMappedVoltage = 255;
-int minMappedVoltage = 0;
+const int maxMappedVoltage = 255;
+const int minMappedVoltage = 0;
 
-int maxUnmappedValue = 1023;
-int minUnmappedValue = 0; 
+const int maxUnmappedValue = 1023;
+const int minUnmappedValue = 0; 
+
+const int delayTime = 3;
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -30,20 +32,30 @@ void setup() {
 
 void loop() {
   // read the input on analog pins 0,1,2:
-  variableResistorValueForBlueLed = analogRead(variableResistorPinForBlueLed); 
-  variableResistorValueForRedLed = analogRead(variableResistorPinForRedLed);
-  variableResistorValueForGreenLed = analogRead(variableResistorPinForGreenLed);
+ readPotValues();
+ mapValues();
+ lightLED();
+ // delay in between reads for stability
+ delay(delayTime);  
+}
 
-  //We map the values output by the potentiometers which are in the interval [0,1023] to the interval [0,255] (values accepted 
-  //by analogWrite function)
-  voltageBlue = map(variableResistorValueForBlueLed,minUnmappedValue,maxUnmappedValue,minMappedVoltage,maxMappedVoltage);   
-  voltageRed = map(variableResistorValueForRedLed,minUnmappedValue,maxUnmappedValue,minMappedVoltage,maxMappedVoltage);
-  voltageGreen = map(variableResistorValueForGreenLed,minUnmappedValue,maxUnmappedValue,minMappedVoltage,maxMappedVoltage);
+void readPotValues(){
+  blueLedPotentiometer = analogRead(blueLedPotentiometerPin); 
+  redLedPotentiometer = analogRead(redLedPotentiometerPin);
+  greenLedPotentiometer = analogRead(greenLedPotentiometerPin);
+}
 
-  Serial.println(voltageRed);
+//Map the values from potentiometers [0,1023] to [0,255] (values accepted by analogWrite function)
+void mapValues(){
+  voltageBlue = map(blueLedPotentiometer,minUnmappedValue,maxUnmappedValue,minMappedVoltage,maxMappedVoltage);   
+  voltageRed = map(redLedPotentiometer,minUnmappedValue,maxUnmappedValue,minMappedVoltage,maxMappedVoltage);
+  voltageGreen = map(greenLedPotentiometer,minUnmappedValue,maxUnmappedValue,minMappedVoltage,maxMappedVoltage);
+}
+
+
+void lightLED(){
   analogWrite(blueLedPin, voltageBlue);
   analogWrite(redLedPin, voltageRed);
   analogWrite(greenLedPin, voltageGreen);
-
-  delay(3);  // delay in between reads for stability
 }
+
